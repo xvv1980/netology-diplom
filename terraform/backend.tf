cf3dec90604c35987c238aed58ba52b78b52564b
xvv1980 <xvv1980@yandex.ru>
2024-12-05 15:35:00 +0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,12 +4,12 @@ terraform {
                    s3 =       "https://storage.yandexcloud.net"
       }
     region = "ru-central1"
-    bucket = "tf-state-xvv1980/"
+    bucket = "tf-state-xvv1980"
     key = "tf-remote-state-diplom"
     skip_region_validation = true
     skip_credentials_validation = true
     skip_requesting_account_id = true
-    skip_metadata_api_check = false
+    skip_metadata_api_check = true
     skip_s3_checksum = true
   }
 }
