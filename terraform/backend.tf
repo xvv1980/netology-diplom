6da92e01b07b6b4ed7f947460733520a7c1eb93b
xvv1980 <xvv1980@yandex.ru>
2024-12-05 15:00:41 +0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -9,7 +9,7 @@ terraform {
     skip_region_validation = true
     skip_credentials_validation = true
     skip_requesting_account_id = true
-    skip_metadata_api_check = true
+    skip_metadata_api_check = false
     skip_s3_checksum = true
   }
 }
